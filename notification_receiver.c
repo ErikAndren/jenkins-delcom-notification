f02c8217ed349480c9ce19dea64214f8be0ba58d
Erik Zachrisson <erik.zachrisson@aacmicrotec.com>
2016-11-25 16:42:33 +0100

    Silence warnings

--- notification_receiver.c
+++ notification_receiver.c
@@ -1,4 +1,3 @@
-
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -50,18 +49,19 @@ int main() {
   si_me.sin_port = htons(PORT);
   si_me.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  status = bind(s, &si_me, sizeof(si_me));
+  status = bind(s, (const struct sockaddr *) &si_me, sizeof(si_me));
   if (status < 0) {
     printf("Failed to bind to ip address (%d:%s)\n", errno, strerror(errno));
     exit(errno);
   }
 
   while (true) {
-    status = recvfrom(s, buf, BUFLEN, 0, &si_other, &slen);
+    status = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
     if (status < 0) {
       printf("Failed to receive data (%d:%s)\n", errno, strerror(errno));
       exit(errno);
     }
+    printf("%s\n", buf);
 
     substr = strstr(buf, "STARTED");
     if (substr != NULL) {
